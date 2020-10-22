2ab0ce93decc42233e14add51afd490b618bc01f
TheAlroundGamer <35393046+TheAlroundGamer@users.noreply.github.com>
2020-10-22 16:40:08 +0200

    Add files via upload
    Update 3!
    Now with explaining comment in the code

--- Interest calculator.py
+++ Interest calculator.py
@@ -3,12 +3,13 @@ money_in_account = float(input("Money in account:"))
 
 y_or_m = str(input("(y)ears or (m)onths:"))
 
-years = "y" in y_or_m
-months = "m" in y_or_m
+years = "y" in y_or_m and "years" in y_or_m
+months = "m" in y_or_m and "years" in y_or_m
 
+# statements for calculations and output
 if years == True:
     amount_of_years = int(input("Amount of years:"))
-    yearly_interest = float(input("Amount of interest in a year in % (1.xx):"))
+    yearly_interest = float(input("Amount of interest in a year in % (1.10 = 10%):"))
     total_yearly_money = round((money_in_account * yearly_interest ** amount_of_years), 2)
     print("In %s years you'll have $%s of interest" % (amount_of_years, (total_yearly_money - money_in_account)))
     if yearly_interest < 1.01:
@@ -22,7 +23,7 @@ if years == True:
 
 elif months == True:
     amount_of_months = int(input("Amount of months:"))
-    monthly_interest = float(input("Amount of interest in a month in % (1.xx):"))
+    monthly_interest = float(input("Amount of interest in a month in % (1.10 = 10%):"))
     total_monthly_money = round((money_in_account * monthly_interest ** amount_of_months), 2)
     print("In %s years you'll have $%s of interest" % (amount_of_months, (total_monthly_money - money_in_account)))
     if monthly_interest < 1.01:
